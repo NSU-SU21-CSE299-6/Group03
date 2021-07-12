4e423864e6c9910c76c9c743bd43cf57dec7dd5c
Hosnara Happy <hosnara.happy@northsouth.edu>
2021-07-12 13:10:35 +0600

    Added bootstrap components #8
    Added Card component to latest product section and minor changes made.

--- Code/Project/frontend/src/components/Home.js
+++ Code/Project/frontend/src/components/Home.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className="container container-fludi">
+        <div className="container container-fluid">
             <h1 id="products_heading">Latest Products</h1>
         <section id="products" class="container mt-5">
          <div className="row">
              <div className="col-sm-12 col-md-6 col-lg-3 my-3">
                <div className="indoor_plant p-3 rounded">
                   <img 
-                  id="indoor_money_plant"
-                  className="indoor_plant-img-top mx-auto"
+                  id="indoor_plant"
+                  className="card-img-top mx-auto"
                   src="images/indoor_money_plant.jpg"
                 />
-                <div className="indoor_plant-body d-flex flex-column">
-                 <h5 className="indoor_plant-title">
+                <div className="card-body d-flex flex-column">
+                 <h5 className="card-title">
                      <a href="">Indoor_money_plant</a>
                  </h5>
                  <div className="ratings mt-auto">
