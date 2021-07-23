c3e21cb7e9830a267207f569bcf63c551fd02970
Ahmed Omar <ahmed.adnan01@northsouth.edu>
2021-07-23 20:48:25 +0600

    BackEnd #11 : Handled uncaught exceptions
    if there is any uncaught exception errors (errors in the code), then it will now automatically catch it, close the server, and throw a feedback about where the error exist.

--- Code/Project/backend/server.js
+++ Code/Project/backend/server.js
@@ -3,14 +3,33 @@ const connectDatabase = require('./config/database')
 
 const dotenv = require('dotenv');
 
+// Handle uncaught exceptions
+process.on('uncaughtException', err => {
+  let x = err.stack
+  console.log('Error: ' + x)
+  console.log('Shutting down server due to uncaught exception')
+  process.exit(1)
+})
+
 //Setting up config file
 dotenv.config({ path: 'Group03/Code/Project/backend/config/config.env' })
 
 //Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   let x = process.env.PORT;
   let y = process.env.NODE_ENV;
   console.log('Server started on PORT: ' + x + ' in ' + y + ' mode.')
+})
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', err => {
+  let x = err.message;
+  console.log('ERROR: ' + x)
+  console.log('Shutting down the server due to Unhandled Promise rejection');
+  server.close(() => {
+    process.exit(1)
+  })
 })
