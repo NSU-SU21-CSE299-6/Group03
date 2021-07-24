bddffc3c2007454db8cd9852d46d9d2aa69a654b
Hosnara Happy <hosnara.happy@northsouth.edu>
2021-07-24 15:20:32 +0600

    FrontEnd #19 : Displayed product name, images, number of reviews from redux state
    Previous dummy image is removed and now displaying product name, images, number of reviews from the database

--- Code/Project/frontend/src/components/Home.js
+++ Code/Project/frontend/src/components/Home.js
@@ -7,6 +7,9 @@ import { getProducts } from '../actions/productActions'
 const Home = () => {
 
     const dispatch = useDispatch();
+
+    const { loading, products, errors} = useSelector(state => state.products)
+
     useEffect(() => {
        dispatch(getProducts());
 
@@ -18,28 +21,33 @@ const Home = () => {
             <h1 id="products_heading">Latest Products</h1>
         <section id="products" className="container mt-5">
          <div className="row">
-             <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+             {products && products.map(product => (
+
+<div key={product.id}className="col-sm-12 col-md-6 col-lg-3 my-3">
                <div className="indoor_plant p-3 rounded">
                   <img 
                   id="indoor_plant"
                   className="card-img-top mx-auto"
-                  src="images/indoor_money_plant.jpg"
+                  src={product.images[0].url}
                 />
                 <div className="card-body d-flex flex-column">
                  <h5 className="card-title">
-                     <a href="">Indoor_money_plant</a>
+                     <a href="">{product.name}</a>
                  </h5>
                  <div className="ratings mt-auto">
                      <div className="rating-outer">
                          <div className="rating-inner"></div>
                      </div>
-                   <span id="no-of-reviews">(Reviews)</span>
+                   <span id="no-of-reviews">({product.numOfReviews}Reviews)</span>
                  </div>
                     <p className="indoor_plant-text">(price)</p>
                     <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                 </div>
                </div>
              </div>
+             ))}
+
+             
 
          </div>
         </section>
