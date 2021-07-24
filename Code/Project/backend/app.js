05663fe36f1b1f370b1beda5e4fa002f413735ba
Sazzad Alam <baazifly@gmail.com>
2021-07-24 10:30:33 +0600

    BackEnd #5 created auth.js , authController.js
    new route called auth.js added into app.js to  try out user registration.

--- Code/Project/backend/app.js
+++ Code/Project/backend/app.js
@@ -6,9 +6,11 @@ const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json());
 
-const products = require('./routes/product')
+const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use('/api/v1', products)
+app.use('/api/v1', auth)
 
 app.use(errorMiddleware);
 
