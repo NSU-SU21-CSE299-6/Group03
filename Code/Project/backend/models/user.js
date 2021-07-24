5de03613c59bc1945fb701ff95bd34d99a8deadb
Sazzad Alam <baazifly@gmail.com>
2021-07-24 09:50:45 +0600

    BackEnd #5 Created User Model
    Created a user model to register user and encrypt their password. I've used mongoose to create a userSchema and created schemas to store User name email and  password. Also added a option to reset forgot password using a jswebtoken.

---
+++ Code/Project/backend/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength: [30, 'Your name can not exceed 30 Characters']
+    },
+
+    email: {
+        type:String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        validate: [validator.isEmail,'Please enter valid email address']
+    },
+
+    password:{
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6,'Your password cannot be less than 6 characters'],
+        select: false
+
+    },
+
+    avatar: {
+        public_id: {
+            type:String,
+            required: true
+        },
+        url:{
+            type:String,
+            required: true
+        }
+    },
+
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+
+    resetPasswordToken: String,
+    resetpasswordExpire: Date
+
+})
+
+module.exports = mongoose.model('User', userSchema);
