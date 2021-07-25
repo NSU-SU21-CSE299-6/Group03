2b43a67056568a48fb1db78420b526182e0ac259
Sazzad Alam <baazifly@gmail.com>
2021-07-25 09:53:40 +0600

    BackEnd #5 JWT token store as cookie
    Now the user details will store as http cookie so that it can  not be accessed from frontend. To do that JWT token has been used with a expire date which will be assigned to every new registered member and every new login.

--- Code/Project/backend/controllers/authController.js
+++ Code/Project/backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const Errorhandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // Register a user => /api/v1/register
 
@@ -19,12 +20,8 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user,  200, res)
+ 
 })
 
 // user login route> /api/v1/login
@@ -50,10 +47,5 @@ exports.loginUser = catchAsyncErrors( async(req, res, next) => {
         return next(new Errorhandler('Invalid username or Password',401));
     }
 
-    const token = user.getJwtToken();
-
-    res.status(200).json({
-        success: true,
-        token
-    })
+    sendToken(user,  200, res)
 })
---
+++ Code/Project/backend/utils/jwtToken.js
@@ -0,0 +1,24 @@
+//create, send token and save cookie
+
+const sendToken = (user, statuscode, res ) => {
+
+    //create token
+    const token = user.getJwtToken();
+
+    //cookie
+    const options  = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+    res.status(statuscode).cookie('token', token, options).json({
+        success: true,
+        token,
+        user
+    })
+
+}
+
+module.exports = sendToken;
