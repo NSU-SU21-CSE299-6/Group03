e4e06ec4c066d1b9b29e7b7a71c77789c5a62df2
Sazzad Alam <baazifly@gmail.com>
2021-07-28 09:52:56 +0600

    BackEnd #5 Admin role authorization
    To delete, update or putting up new products it will require admin authorization from now on. User can not access this resources only admin can.

--- Code/Project/backend/routes/product.js
+++ Code/Project/backend/routes/product.js
@@ -7,16 +7,17 @@ const{ getProducts,
   updateProduct, 
   deleteProduct } = require('../controllers/productController')
 
-  const {isAuthenticatedUser} = require('../middlewares/auth');
+  const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(isAuthenticatedUser,newProduct);
+//admin routes
+router.route('/admin/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newProduct);
 
 router.route('/admin/product/:id')
-.put(isAuthenticatedUser,updateProduct)
-.delete(isAuthenticatedUser,deleteProduct)
+.put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct)
+.delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct);
 
 
 module.exports = router;
