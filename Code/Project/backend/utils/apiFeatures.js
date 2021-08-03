c1c2edb76ea7f905530e48c4b699f723bce4f355
Ahmed Omar <ahmed.adnan01@northsouth.edu>
2021-08-04 05:59:30 +0600

    BackEnd #18 : Added filter function by price range
    added a feature to our API so that we can filter products in price ranges

--- Code/Project/backend/utils/apiFeatures.js
+++ Code/Project/backend/utils/apiFeatures.js
@@ -27,7 +27,11 @@ class APIFeatures {
     const removeFields = ['keyword']
     removeFields.forEach(el => delete queryCopy[el]);
 
-    this.query = this.query.find(queryCopy);
+    //filter by price
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
 
 
