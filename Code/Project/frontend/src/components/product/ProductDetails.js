4e1acdf50acf861fb913ec2988ff9082757aafea
Hosnara Happy <hosnara.happy@northsouth.edu>
2021-08-15 16:29:27 +0600

    FrontEnd #9 : Added Quantity Selection on Product Details page
    Now, we can add and minus Product quantity on cart from Product Details page.

--- Code/Project/frontend/src/components/product/ProductDetails.js
+++ Code/Project/frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import {useAlert} from 'react-alert'
 import Loader from '../layout/Loader'
 import MetaData from '../layout/MetaData'
@@ -9,6 +9,8 @@ import { Carousel } from 'react-bootstrap'
 
 const ProductDetails = ({ match}) => {
    
+        const [quantity, setQuantity] = useState(1)
+
     const dispatch = useDispatch();
 
     const { loading, error, product } = useSelector(state => state.productDetails)
@@ -23,6 +25,22 @@ const ProductDetails = ({ match}) => {
       }
 
     }, [dispatch, alert, error, match.params.id])
+
+    const increaseQty = () => {
+        const count = document.querySelector('.count')
+
+        if(count.valueAsNumber >= product.stock) return;
+        const qty = count.valueAsNumber +1;
+        setQuantity(qty)
+    }
+
+    const decreaseQty = () => {
+        const count = document.querySelector('.count')
+
+        if(count.valueAsNumber <= 1) return;
+        const qty = count.valueAsNumber - 1;
+        setQuantity(qty)   
+    }
 
     let product_rating = (product.ratings/ 5) * 100
     let productRatings = product_rating + "%"
@@ -59,8 +77,14 @@ const ProductDetails = ({ match}) => {
                             <hr />
 
                             <p id="product_price">Price: {product.price} TK</p>
-                            
+                            <div className="stockCounter d-inline">
+                                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
+                                <input type="number" className="form control count d-inline"
+                                 value={quantity} readOnly />
+                                 <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
+                           </div>
+                           <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
                             <hr />
 
                             <p>Status: <span id="stock_status" className={product.stock > 0 ? 'greenColor' : 'redColor'} >
