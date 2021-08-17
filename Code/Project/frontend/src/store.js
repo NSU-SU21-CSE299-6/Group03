624f6008043dd8677621a42a546748a5a58a0f32
Sazzad Alam <baazifly@gmail.com>
2021-08-18 03:44:46 +0600

    Revert "FrontEnd #29 : Created User Login Page"
    This reverts commit a6fdae736a864c1d66299eebfaf3cf3fe64160d1.

--- Code/Project/frontend/src/store.js
+++ Code/Project/frontend/src/store.js
@@ -4,12 +4,10 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productsReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers';
-import { authReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
    products: productsReducer,
    productDetails: productDetailsReducer,
-   auth: authReducer,
    cart: cartReducer
 })
 
