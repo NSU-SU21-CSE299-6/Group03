c1dd1a12624aff1a1536e7d0d12e1e4ee6be3eed
Ahmed Omar <ahmed.adnan01@northsouth.edu>
2021-08-22 15:23:43 +0600

    FrontEnd #33 : minor bug fixed
    Minor bug fixed due to which login wasn't being performed correctly

--- Code/Project/frontend/src/actions/userActions.js
+++ Code/Project/frontend/src/actions/userActions.js
@@ -17,7 +17,7 @@ export const login = (email, password) => async (dispatch) => {
       }
     }
 
-    const { data } = await axios.post('/api/v1/login, {email, password}', config)
+    const { data } = await axios.post('/api/v1/login', { email, password }, config)
 
     dispatch({ 
       type: LOGIN_SUCCESS,
