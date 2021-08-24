298b098c23266555385bdc81ce39f5d5039a1594
Ahmed Omar <ahmed.adnan01@northsouth.edu>
2021-08-24 15:02:56 +0600

    FrontEnd #39 : Implemented logout feature
    Disregarded previous plan of using dropdown, and implemented logout feature in the header

--- Code/Project/frontend/src/components/layout/Header.js
+++ Code/Project/frontend/src/components/layout/Header.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 import { Route, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { useAlert } from 'react-alert'
+import { logout } from '../../actions/userActions'
 
 import Search from './Search'
 
@@ -14,6 +15,13 @@ const Header = () => {
     const { user, loading } = useSelector(state => state.auth)
 
     const { cartItems } = useSelector(state => state.cart)
+
+    const logoutHandler = () => {
+        dispatch(logout());
+        alert.success('Logged Out Successfully');
+    }
+
+
 
     return (
         <Fragment>
@@ -39,9 +47,9 @@ const Header = () => {
 
             {user ? (
 
-                <div className="ml-4 dropdown d-inline">
+                <div className="ml-4 d-inline">
 
-                            <div><Link to="#!" className="btn dropdown-toggle mr-4" type="button" id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div className="btn mr-4" type="button">
 
                                 <figure className="avatar avatar-nav">
                                     <img
@@ -50,16 +58,15 @@ const Header = () => {
                                         className="rounded-circle"
                                     />
                                 </figure>
-                                <span>{user && user.name}</span>
-                            </Link></div>
+                                <span id="header_name">{user && user.name}</span>
+                                <span className="text-danger p-2" to="/" onClick={logoutHandler}>
+                                    Logout
+                                </span>
+                            </div>
 
                             <div className="dropdown-menu" aria-labelledby="dropDownMenuButton">
 
-                                {user && user.role === 'admin' && (
-                                    <Link className="dropdown-item" to="/dashboard">Dashboard</Link>
-                                )}
-                                <Link className="dropdown-item" to="/orders/me">Orders</Link>
-                                
+
                                 
 
                             </div>
